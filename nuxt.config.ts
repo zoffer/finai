bfa460c8df5ba7e02060055fce05778e445bd765
zoffer <zoffer@foxmail.com>
2025-12-12 14:01:24 +0800

    chore: 移除PWA相关配置和依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,32 +6,10 @@ export default defineNuxtConfig({
   // https://nuxt.com/docs/4.x/api/nuxt-config#runtimeconfig-1
   vite: { build: { target: "es2015" } },
 
-  pwa: {
-    devOptions: { enabled: true },
-    manifest: {
-      name: "finai",
-      short_name: "finai",
-      theme_color: "#ffffff",
-      background_color: "#ffffff",
-      display: "standalone",
-      icons: [
-        {
-          src: "/android-chrome-192x192.png",
-          sizes: "192x192",
-          type: "image/png"
-        }, {
-          src: "/android-chrome-512x512.png",
-          sizes: "512x512",
-          type: "image/png"
-        },
-      ],
-    }
-  },
-
   experimental: {
     // https://nuxt.com/docs/4.x/guide/going-further/experimental-features#entryimportmap
     entryImportMap: false,
   },
 
-  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+  modules: ["@nuxtjs/tailwindcss"],
 })
