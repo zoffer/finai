2671e26c36c0a034cb0626cc61a5dc9baaa8f6fd
zoffer <zoffer@foxmail.com>
2026-01-27 13:53:02 +0800

    perf(股票历史数据): 添加请求队列和缓存机制
    引入 p-queue 限制并发请求以避免 API 限流
    添加 Redis 缓存机制减少重复请求
    设置缓存过期时间为 1 小时

--- server/api/stock/history/index.get.ts
+++ server/api/stock/history/index.get.ts
@@ -4,14 +4,42 @@ import type { H3Event } from "h3";
 import { tStock } from "~~/drizzle/schema/stock";
 import z from "zod";
 import { crawlStockHistory } from "~~/server/utils/data-source/arktools/hist";
+import { rd } from "~~/server/utils/redis";
 
 const zParameter = z.object({
     id: z.string().trim(),
 });
 
+const zData = z.array(
+    z.object({
+        time: z.string(),
+        open: z.number(),
+        close: z.number(),
+        high: z.number(),
+        low: z.number(),
+        volume: z.number(),
+        turnover: z.number(),
+        amplitude: z.number(),
+        change: z.number(),
+        change_amount: z.number(),
+        turnover_rate: z.number(),
+    })
+);
+
 export default defineApiEventHandler(async (event: H3Event<{ query: z.input<typeof zParameter> }>) => {
     // 获取股票代码参数
     const query = await apiParameterParse(zParameter, getQuery(event));
+
+    // 从缓存中获取数据
+    try {
+        const cachedData = await getData(query.id);
+        if (cachedData) {
+            return { data: cachedData };
+        }
+    } catch (error) {
+        console.error("获取缓存数据错误", error);
+    }
+
     const [stock] = await db
         .select({
             id: tStock.id,
@@ -40,5 +68,31 @@ export default defineApiEventHandler(async (event: H3Event<{ query: z.input<type
         ...item,
     }));
 
+    cacheData(query.id, data);
+
     return { data };
 });
+
+const CACHE_KEY = "stock:history:cache:H";
+
+async function getData(id: string) {
+    const cachedData = await rd.HGET(CACHE_KEY, id);
+    if (cachedData) {
+        return zData.parse(JSON.parse(cachedData));
+    }
+}
+
+async function cacheData(id: string, data: z.infer<typeof zData>) {
+    await rd.HSETEX(
+        CACHE_KEY,
+        {
+            [id]: JSON.stringify(data),
+        },
+        {
+            expiration: {
+                type: "EX",
+                value: 60 * 60,
+            },
+        }
+    );
+}
